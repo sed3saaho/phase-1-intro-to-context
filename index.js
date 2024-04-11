b8594402a40e7eda2b8c40f3732cca8f3f8fd382
Sedly <sedly.keith@student.moringaschool.com>
2024-04-11 17:49:25 +0300

    phase-1-intro-to-context

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Your code here
+const createEmployeeRecord = (row) => {
+    return {
+      firstName: row[0],
+      familyName: row[1],
+      title: row[2],
+      payPerHour: row[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+  }
+  
+  const createEmployeeRecords = (employeeRowData) => {
+    return employeeRowData.map((row) => createEmployeeRecord(row));
+  }
+  
+  const createTimeInEvent = (employee, dateStamp) => {
+    const [date, hour] = dateStamp.split(' ');
+  
+    employee.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date,
+    });
+  
+    return employee;
+  }
+  
+  const createTimeOutEvent = (employee, dateStamp) => {
+    const [date, hour] = dateStamp.split(' ');
+  
+    employee.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date,
+    });
+  
+    return employee;
+  }
+  
+  const hoursWorkedOnDate = (employee, soughtDate) => {
+    const inEvent = employee.timeInEvents.find((e) => e.date === soughtDate);
+    const outEvent = employee.timeOutEvents.find((e) => e.date === soughtDate);
+  
+    return (outEvent.hour - inEvent.hour) / 100;
+  }
+  
+  const wagesEarnedOnDate = (employee, dateSought) => {
+    const rawWage = hoursWorkedOnDate(employee, dateSought) * employee.payPerHour;
+    return parseFloat(rawWage.toString());
+  }
+  
+  const allWagesFor = (employee) => {
+    const eligibleDates = employee.timeInEvents.map((e) => e.date);
+  
+    const payable = eligibleDates.reduce((memo, d) => memo + wagesEarnedOnDate(employee, d), 0);
+  
+    return payable;
+  }
+  
+  const findEmployeeByFirstName = (srcArray, firstName) => {
+    return srcArray.find((rec) => rec.firstName === firstName);
+  }
+  
+  const calculatePayroll = (arrayOfEmployeeRecords) => {
+    return arrayOfEmployeeRecords.reduce((memo, rec) => memo + allWagesFor(rec), 0);
+  }
